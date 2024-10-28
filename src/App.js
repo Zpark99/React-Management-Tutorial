90a44a386bbbf054bc25f62112aad1558eba4c29
Zpark99 <qkrwlswo96@naver.com>
2024-10-28 22:47:29 +0900

    Create Custmoer.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
 // 웹사이트 출력 내용을 담당하는 앱 
 
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer';
 import './App.css';
 
-class App extends Component {
-  render() {
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '박진재',
+    'birthday': '960530',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '김영기',
+    'birthday': '950530',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '박민',
+    'birthday': '940530',
+    'gender': '남자',
+    'job': '대학생'
+  },
+]
+
+class App extends Component { //Component: app 를 그릴 수 있는 최소 단위
+  render() { //render는 return 구문을 써서 반환 
     return (
-      <div className="gray-background">
-        <img src={logo} alt="logo" />
-        <h2>Let's develop management system!</h2>
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer 
+                key={c.id} // 원래이거 넣어줘야함 
+                id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}              
+              /> ); }) }
       </div>
     );
   }
