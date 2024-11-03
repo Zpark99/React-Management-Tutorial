5697085b04ac0bfe8a2329c47ec38616f279d19c
Zpark99 <qkrwlswo96@naver.com>
2024-11-03 21:03:24 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@
 import React, { Component } from 'react';
 import Customer from './components/Customer';
 import './App.css';
+import Paper from '@mui/material/Paper';
+import { Table, TableHead, TableBody, TableRow, TableCell } from '@mui/material';
+import { styled } from '@mui/system'; // MUI v5 styled API 사용, 최신버전 사용한다는 뜻
+
+const StyledPaper = styled(Paper)(({ theme }) => ({
+  width: '100%',
+  margin: theme.spacing(3),
+  overflowX: "auto"
+}));
+
+const StyledTable = styled(Table)(({ theme }) => ({
+  minWidth: 1080,
+}));
 
 const customers = [
   {
@@ -34,15 +47,23 @@ const customers = [
 class App extends Component { //Component: app 를 그릴 수 있는 최소 단위
   render() { //render는 return 구문을 써서 반환 
     return (
-      <div>
-        {
-          customers.map(c => {
-            return (
-              <Customer 
-                key={c.id} // 원래이거 넣어줘야함 
-                id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}              
-              /> ); }) }
-      </div>
+      <StyledPaper>
+        <StyledTable>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell> {/* 중복 제거 */}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(c => { return ( <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ); }) }
+          </TableBody>
+        </StyledTable>
+      </StyledPaper>
     );
   }
 }
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,37 +1,40 @@
 import React from "react";
+import { TableRow, TableCell } from '@mui/material';
+import PropTypes from 'prop-types'; // 유효성 검사 
 
 class Customer extends React.Component {
   render() {
     return ( //코드 반환을 위해 return 추가, props 저건 지금은 문제 없긴함 
-      <div>
-          <CustomerProfile id={this.props.id} image={this.props.image}/>
-          <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job} />
-      </div>
+        <TableRow>
+          <TableCell>{this.props.id}</TableCell>
+          <TableCell><img src={this.props.image} alt="profile"/> </TableCell>
+          <TableCell>{this.props.name}</TableCell>
+          <TableCell>{this.props.birthday}</TableCell>
+          <TableCell>{this.props.gender}</TableCell>
+          <TableCell>{this.props.job}</TableCell>
+        </TableRow>
     );
   }
 }
 
-class CustomerProfile extends React.Component {
-  render() {
-    return (
-      <div>
-        <img src={this.props.image} alt="profile"/>
-        <h2>{this.props.name}({this.props.id})</h2>
-      </div>
-    )
-  }
-}
+// class CustomerProfile extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <img src={this.props.image} alt="profile" style={{ width: '64px', height: '64px' }}/>
+//         <h2>{this.props.name}({this.props.id})</h2>
+//       </div>
+//     )
+//   }
+// }
 
-class CustomerInfo extends React.Component {
-  render() {
-      return (
-        <div>
-            <p>{this.props.birthday}</p>
-            <p>{this.props.gender}</p>
-            <p>{this.props.job}</p>
-        </div>
-    )
-  }
-}
+Customer.propTypes = {
+  id: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  birthday: PropTypes.string.isRequired,
+  gender: PropTypes.string.isRequired,
+  job: PropTypes.string.isRequired,
+};
 
 export default Customer;
